88a6d8a1ffd3abaafcbbebacce41b6ec56dc5e86
WangZY <76583642+xmx@users.noreply.github.com>
2026-01-06 10:12:25 +0800

    提交到主分支

--- machine_id_test.go
+++ machine_id_test.go
@@ -1,11 +1,28 @@
 package tunnel
 
-import "testing"
+import (
+	"fmt"
+	"log/slog"
+	"testing"
+)
 
 func TestIdent(t *testing.T) {
-	ident := NewIdent("", nil)
-	for i := 0; i < 10; i++ {
+	log := slog.Default()
+	ident := NewMachineID("", &outLog{log: log})
+	for i := 0; i < 1; i++ {
 		id := ident.MachineID(true)
 		t.Log("machineID:", id)
 	}
+}
+
+type outLog struct {
+	log *slog.Logger
+}
+
+func (o *outLog) Infof(s string, a ...any) {
+	o.log.Info(fmt.Sprintf(s, a...))
+}
+
+func (o *outLog) Warnf(s string, a ...any) {
+	o.log.Warn(fmt.Sprintf(s, a...))
 }
